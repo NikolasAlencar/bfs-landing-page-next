7fe661a39f7b8583b77a745db8aebe2050d18805
Nikolas Alencar <nikolasalencarrs73@gmail.com>
2025-12-02 21:04:06 -0300

    feat: cria metadata pra title e description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import { Footer } from "@/components/Footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 import "./globals.css";
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Business Fast Solutions',
+  description: 'Paralegal, Contabilidade e Legalização na Velocidade que o Seu Negócio Exige',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
